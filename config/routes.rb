c2606c7f9b9f30c1b2476d3c2622f8e418c3cf25
andrea longhi <andrea@spaghetticode.it>
2020-02-12 11:23:57 +0100

    Deprecate Spree::StripeController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Spree::Core::Engine.routes.draw do
+  # deprecated route, it uses a deprecated controller:
+  post '/stripe/confirm_payment', to: 'stripe#confirm_payment'
+
   post '/stripe/confirm_intents', to: '/solidus_stripe/intents#confirm'
   post '/stripe/shipping_rates', to: '/solidus_stripe/payment_request#shipping_rates'
   post '/stripe/order_update', to: '/solidus_stripe/payment_request#update_order'
